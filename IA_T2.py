1be642bdc89064a1c4af50a031f4e4af3d02415e
igor-kevin <33360999+igor-kevin@users.noreply.github.com>
2017-11-08 11:41:20 -0200

    naruto fanboy
    Tirei os .copy() pq nao funciona no python 2, ai tive que ajustar o código.
    Ajustei os números dos inimigos, poços e da barra de ouro que vale mais do que dinheiro.

--- IA_T2.py
+++ IA_T2.py
@@ -1,25 +1,28 @@
+import random
 def cria_caverna():
     
     
     lista_locais_usados = []
     Tabuleiro = []
     coluna = []
-    #esses são para definir as paredes da caverna
+    inimigos_20 = inimigos_50 = gold_ingodo = BURACAO =0 
+    #esses sao para definir as paredes da caverna
     for i in range(14):
         coluna.append(1)
-    Tabuleiro.append(coluna.copy())
-    coluna = []
-    coluna.append(1)
-    for i in range (12):
-        coluna.append(0)
-    coluna.append(1)
+    Tabuleiro.append(coluna)
     for i in range(12):
-        Tabuleiro.append(coluna.copy())
+        coluna = []
+        coluna.append(1)
+        for i in range (12):
+            coluna.append(0)
+        coluna.append(1)
+        Tabuleiro.append(coluna)
+        
         
     coluna = []
     for i in range(14):
         coluna.append(1)
-    Tabuleiro.append(coluna.copy())
+    Tabuleiro.append(coluna)
     
     #colocar os inimigos, buracos e barras de ouros que valem mais do que dinheiro
     
@@ -30,6 +33,64 @@ def cria_caverna():
 
     Tabuleiro[1][1] = -20
     contador=0
+    for i in Tabuleiro:
+        
+        print i
+    #cria os inimigos que dao 20 de dano
+    while(inimigos_20 <2):
+        new_local=[]
+        col_num = random.randint(1,12)
+        lin_num = random.randint(1,12)
+        new_local.append(col_num)
+        new_local.append(lin_num)
+        if new_local not in lista_locais_usados:
+            copy_novo = new_local
+            lista_locais_usados.append(copy_novo)
+            print(lista_locais_usados)
+            Tabuleiro[lin_num][col_num] = Tabuleiro[lin_num][col_num]+100
+            inimigos_20 = inimigos_20+1
+    # cria os inimigos que dao 50 de dano
+    while(inimigos_50<2):
+        new_local=[]
+        col_num = random.randint(1,12)
+        lin_num = random.randint(1,12)
+        new_local.append(col_num)
+        new_local.append(lin_num)
+        if new_local not in lista_locais_usados:
+            copy_novo = new_local
+            lista_locais_usados.append(copy_novo)
+            print(lista_locais_usados)
+            Tabuleiro[lin_num][col_num] = Tabuleiro[lin_num][col_num]+500
+            inimigos_50 = inimigos_50+1
+    #cria os buracos
+    while(BURACAO<8):
+        new_local=[]
+        col_num = random.randint(1,12)
+        lin_num = random.randint(1,12)
+        new_local.append(col_num)
+        new_local.append(lin_num)
+        if new_local not in lista_locais_usados:
+            copy_novo = new_local
+            lista_locais_usados.append(copy_novo)
+            print(lista_locais_usados)
+            Tabuleiro[lin_num][col_num] = Tabuleiro[lin_num][col_num]-300
+            BURACAO = BURACAO+1
+    #cria as barras de ouro que valem mais do que dinheiro
+    while(gold_ingodo<3):
+        new_local=[]
+        col_num = random.randint(1,12)
+        lin_num = random.randint(1,12)
+        new_local.append(col_num)
+        new_local.append(lin_num)
+        if new_local not in lista_locais_usados:
+            copy_novo = new_local
+            lista_locais_usados.append(copy_novo)
+            print(lista_locais_usados)
+            Tabuleiro[lin_num][col_num] = Tabuleiro[lin_num][col_num]+8000
+            gold_ingodo = gold_ingodo+1
+
+    '''
+    me ajudou a fazer os bixo e tal, deixa salvo pra ajudar depois
     while (contador <15):
         new_local=[]
         col_num = random.randint(1,12)
@@ -37,14 +98,14 @@ def cria_caverna():
         new_local.append(col_num)
         new_local.append(lin_num)
         if new_local not in lista_locais_usados:
-            copy_novo = new_local.copy()
+            copy_novo = new_local
             lista_locais_usados.append(copy_novo)
             print(lista_locais_usados)
-            Tabuleiro[lin_num][col_num] = contador+100
+            Tabuleiro[lin_num][col_num] = Tabuleiro[lin_num][col_num]+100
             contador = contador+1
     
-            
-    #protótipo de brisa e as paradas
+                
+    protótipo de brisa e as paradas, me ajudou a colocar as brisas e mal cheiros
     for i in range(13):
         for j in range(13):
             if Tabuleiro[i][j]>30:
@@ -52,10 +113,23 @@ def cria_caverna():
                 Tabuleiro[i][j-1] = Tabuleiro[i][j-1]+7
                 Tabuleiro[i+1][j] = Tabuleiro[i+1][j]+7
                 Tabuleiro[i][j+1] = Tabuleiro[i][j+1]+7
-            
-            
+    '''        
+
+    for i in range(13):
+        for j in range(13):
+            if Tabuleiro[i][j] >-400 and Tabuleiro[i][j] < -100:
+                Tabuleiro[i-1][j] = Tabuleiro[i-1][j]+13
+                Tabuleiro[i][j-1] = Tabuleiro[i][j-1]+13
+                Tabuleiro[i+1][j] = Tabuleiro[i+1][j]+13
+                Tabuleiro[i][j+1] = Tabuleiro[i][j+1]+13
+                
+            if Tabuleiro[i][j] >80 and Tabuleiro[i][j] < 700:
+                Tabuleiro[i-1][j] = Tabuleiro[i-1][j]+7
+                Tabuleiro[i][j-1] = Tabuleiro[i][j-1]+7
+                Tabuleiro[i+1][j] = Tabuleiro[i+1][j]+7
+                Tabuleiro[i][j+1] = Tabuleiro[i][j+1]+7
+                
     for i in Tabuleiro:
-        
-        print(i)
+        print i
 
 cria_caverna()
