244ac7ca4e2342d3cfc62ca93fa7265a46924138
LuiisClaudio <luisclaudio97@hotmail.com>
2017-11-13 18:46:40 -0200

    Retirando os erros, porém ainda tem muito warning

--- Mundo_do_Wumpus.pl
+++ Mundo_do_Wumpus.pl
@@ -18,6 +18,10 @@
 :- dynamic nao_tem_poco/2.
 :- dynamic tem_inimigo/2.
 :- dynamic tem_poco/2.
+:- dynamic inicio/2.
+:- dynamic powerup/2.
+:- dynamic ouro/2.
+:- dynamic inimigo/4.
 
 
 adjacente(X, Y, XX, Y) :- XX is X+1, pode_ser_acessada(XX, Y).
@@ -45,98 +49,6 @@ arqueiro_andar_direcao(X, Y) :- local_arqueiro(XX, YY, Direcao), Y is YY-1, XX =
 arqueiro_andar_direcao(X, Y) :- local_arqueiro(XX, YY, Direcao), X is XX+1, YY = Y, Direcao=leste, !.
 arqueiro_andar_direcao(X, Y) :- local_arqueiro(XX, YY, Direcao), Y is YY+1, XX = X, Direcao=sul, !.
 arqueiro_andar_direcao(X, Y) :- local_arqueiro(XX, YY, Direcao), X is XX-1, YY = Y, Direcao=oeste, !.
-
-
-
-
-
-
-
-
-
-
-%Movimentos possiveis
-
-proximo_movimento(sair) :- local_arqueiro(X, Y, _), inicio(X, Y), exiting(1), !.
-
-
-
-proximo_movimento(morreu) :- energia(E), E < 1,format('Regra 1'), !.
-
-
-
-proximo_movimento(pegar_ouro) :- local_arqueiro(X, Y, _), ouro(X, Y), retract(ouro(X, Y)), assert(vazia(X, Y)),
-					     mario_somar_score(1000),format('Regra 2'), !. 
-
-
-proximo_movimento(Acao) :- caminhoAtual([]), retract(caminhoAtual([])), proximo_movimento(Acao),format('Regra 3'), !.
-
-proximo_movimento(girar) :- caminhoAtual([[X, Y]|_]), not(mario_andar_para(X, Y)), mario_girar(),format('Regra 4'), !.
-
-proximo_movimento(andar) :- caminhoAtual([[X, Y]|T]), mario_andar_para(X, Y),
-					 retractall(caminhoAtual(_)), assert(caminhoAtual(T)), mario_andar(X, Y),format('Regra 5'), !.
-
-
-
-proximo_movimento(girar) :- mario_andar_para(X, Y), not(pode_ser_acessada(X, Y)), mario_girar(),format('Regra 6'), !.
-
-
-proximo_movimento(andar) :- mario_andar_para(X, Y), pode_ser_acessada(X, Y), not(percebeu_algum_perigo()),
-					 not(visitadas(X, Y)), mario_andar(X, Y),format('Regra 7'), !.
-					 
-
-proximo_movimento(girar) :- local_arqueiro(X, Y, _), adjacente(X, Y, XX, YY), not(percebeu_algum_perigo()),
-					   not(visitadas(XX,YY)), not(mario_andar_para(XX, YY)), mario_girar(),format('Regra 8'), !.
-
-
-
-proximo_movimento(Acao) :- tomar_decisao_segura(), proximo_movimento(Acao), writef('Vai por um caminho seguro!\n'),format('Regra 9'), !.
-
-
-
-proximo_movimento(Resultado) :- municao(A), A > 0, local_arqueiro(X, Y, _), adjacente(X, Y, XX, YY), tem_inimigo(XX, YY), mario_andar_para(XX, YY),
-					   energia(E), E > 50, writef('Vai atirar no inimigo!\n'), atirar(Resultado),format('Regra 10'), !.
-				   
-proximo_movimento(girar) :- municao(A), A > 0, local_arqueiro(X, Y, _), adjacente(X, Y, XX, YY), tem_inimigo(XX, YY), not(mario_andar_para(XX, YY)),
-					   energia(E), E > 50, mario_girar(),format('Regra 11'), !.
-					   
-proximo_movimento(Acao) :- municao(A), A > 0, energia(E), E > 50, tomar_decisao_lutar(), proximo_movimento(Acao), writef('Vai atirar no inimigo!\n'),format('Regra 12'), !.
-
-	
-
-
-proximo_movimento(andar) :- energia(E), E > 50, mario_andar_para(X, Y), pode_ter_inimigo(X, Y),
-					 mario_andar(X, Y),format('Regra 13'), !.
-
-proximo_movimento(girar) :- energia(E), E > 50, local_arqueiro(CurX, CurY, _), adjacente(CurX, CurY, XX, YY),
-					   pode_ter_inimigo(XX, YY), not(mario_andar_para(XX, YY)), mario_girar(),format('Regra 14'),!.
-					   	 
-proximo_movimento(Acao) :- energia(E), E > 50, tomar_decisao_pode_encontrar_inimigo(), proximo_movimento(Acao), writef('Vai arriscar inimigo!\n'),format('Regra 15'), !.
-					   
-
-
-proximo_movimento(pegar_power_up) :- local_arqueiro(X, Y, _), pegar(X, Y), retract(pegar(X, Y)), assert(vazia(X, Y)),
-					        mario_subtrair_score(1), mario_somar_energia(20),format('Regra 16'), !. 
-
-proximo_movimento(Acao) :- tomar_decisao_pegar(), proximo_movimento(Acao), writef('Going to pick a power up!\n'),format('Regra 17'), !.
-
-
-
-proximo_movimento(andar) :- score(C), C < 1, mario_andar_para(X, Y), pode_ter_poco(X, Y), mario_andar(X, Y), writef('Vai arriscar cair no poco!\n'),format('Regra 18'), !.
-proximo_movimento(girar) :- score(C), C < 1, local_arqueiro(X, Y, _), adjacente(X, Y, XX, YY), 
-					   pode_ter_poco(XX, YY), not(mario_andar_para(XX, YY)), mario_girar(), writef('Vai arriscar cair no poco!\n'),format('Regra 19'), !.
-proximo_movimento(Acao) :- score(C), C < 1, tomar_decisao_pode_cair_poco(), proximo_movimento(Acao), writef('Vai arriscar cair no poco!\n'),format('Regra 20'), !.
-
-
-
-proximo_movimento(Acao) :- assert(exiting(1)), tomar_decisao_sair(), proximo_movimento(Acao), writef('Vai sair!\n'),format('Regra 21'), !.
-
-
-
-
-
-
-
 
 
 %Base de conhecimento
@@ -195,11 +107,9 @@ remover_incertezas_casa_atual() :-
 atualizar_incertezas() :- (
 		(remover_incertezas_casa_atual();1=1),
 		((sentiu_brisa_poco(), descobre_poco_adjacente());1=1),
-		((ouviu_passos_inimigo(), descobre_adjacente_pode_ter_inimigo());1=1),
-		((percebeu_flash_teletransporte(), descobre_adjacente_pode_ter_teletransporte());1=1),
+		(descobre_adjacente_pode_ter_inimigo();1=1),
 		(( not(sentiu_brisa_poco()), remover_incertezas_pocos_adjacentes() );1=1),
-		(( not(ouviu_passos_inimigo()), remover_incertezas_inimigos_adjacentes() );1=1),
-		(( not(percebeu_flash_teletransporte()), remover_incertezas_teletransportes_adjacentes() );1=1),
+		(remover_incertezas_inimigos_adjacentes() ;1=1),
 		((atualizar_certezas_geral());1=1)
 	), !.
 
@@ -285,13 +195,13 @@ arqueiro_perdeu_municao() :- municao(Municao), NewAmmo is Municao - 1, retract(m
 
 
 
+
 %Parte que Kevin fez! Me explica depois please...
-%not(X):-\+X.
+%nao(X):-\+X.
 %adjacente(ponto(X,Y),ponto(XX,YY)):-(XX is X,YY is Y-1);(XX is X,YY is Y+1);(YY is Y,XX is X+1);(YY is Y,XX is X-1),!.
 %pode_ter_poco(X,Y):-
-	%sentiu_brisa_poco(X1,Y1);adjacente(ponto(X1,Y1),ponto(X,Y))!.
+%sentiu_brisa_poco(X1,Y1);adjacente(ponto(X1,Y1),ponto(X,Y)),!.
 %pode_ter_inimigo(X,Y):-
-	%sentiu_fedor_inimigo(X2,Y2);adjacente(ponto(X2,Y2),ponto(X,Y))!.
-
-%naosafe(X,Y):-(not(safo(X,Y)),not(parede(X,Y)),pode_ter_inimigo(X,Y),pode_ter_poco(X,Y)).
+%sentiu_fedor_inimigo(X2,Y2);adjacente(ponto(X2,Y2),ponto(X,Y)),!.
+%naosafe(X,Y):-(nao(safo(X,Y)),nao(parede(X,Y)),pode_ter_inimigo(X,Y),pode_ter_poco(X,Y)).
 
