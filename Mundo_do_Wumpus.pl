04aa671f3daec99b63ef3a6a517b088ebc4220da
igor-kevin <33360999+igor-kevin@users.noreply.github.com>
2017-11-21 04:40:53 -0200

    coloquei uma dynamic nova pra funfa lá na principal

--- Mundo_do_Wumpus.pl
+++ Mundo_do_Wumpus.pl
@@ -27,6 +27,7 @@
 :- dynamic estado/3.
 :- dynamic muda_estado/2.
 :- dynamic sentiu_fedor_in/2.
+:- dynamic passou_voltando/2.
 
 %funcionou
 adjacente(X, Y, XX, Y) :- XX is X+1, pode_ser_acessada(XX, Y).
